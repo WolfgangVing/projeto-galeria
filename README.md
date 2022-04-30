1f8d3c1b7f11b3fae513d1e92c9f9ecdc2d830cc
Yuri Silva <44654440+WolfgangVing@users.noreply.github.com>
2022-04-30 08:44:02 -0300

    Project Galery
    First commit of the project, with dependencies updated
    and changes in webpack.config.js with those updates in mind.

---
+++ README.md
@@ -0,0 +1,29 @@
+# This project intent is to familiarize with the use of Webpack for a an production aplication.
+
+Because the original was with , almost, all dependencies deprecreted I decided to work with their latest stable versions. -And Because I would like to actually do something by myself-.
+
+| Changes | original | mine |
+| --- | --- | --- |
+| bootstrap | 4.1.1 | ^5.1.3 |
+| webpack | 4.8.3 | ^5.72.0 |
+| webpack-cli | 2.1.3 | ^4.9.2 |
+| style-loader | 0.21.0 | ^3.3.1 |
+| sass-loader | 7.0.1 | ^12.6.0 |
+| postcss-loader | 2.1.5 | ^6.2.1 |
+| mini-css-extract-plugin | 0.4.0 | ^2.6.0 
+| jquery | 3.3.1 | ^3.6.0 |
+| font-awesome | 4.7.0 | ^4.7.0 |
+| css-loader | 0.21.11 | ^6.7.1
+| cross-env | 5.1.5 | ^7.0.3
+| copy-webpack-plugin | 4.5.1 | ^10.2.4
+| bootstrap | 4.1.1  | ^5.1.3
+
+
+What has left us(not for good), and what is in the place
+|left |v| welcome? |v|
+| --- |---| --- | ---|
+|popper.js | 1.14.3 | @popperjs/core| ^2.11.5
+|optimize-css-assets-webpack-plugin|4.0.1|css-minimizer-webpack-plugin|^3.4.1|
+|node-sass| 4.12.0| sass | ^1.51.0 |
+|file-loader| 1.11.0 |webpack's module|
+|uglifyjs-webpack-plugin | 1.2.5 | terser-webpack-plugin | 
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './build',
+        port: 9000,
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new CssMinimizerPlugin()
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: __dirname + '/build'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'estilo.css' }),
+        new CopyPlugin([
+            { context: 'src/', from: '**/*.html' },
+            { context: 'src/', from: 'imgs/**/*' }
+        ])
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            type: 'asset/resource'
+        }, {
+            test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+            type: 'asset/resource'
+        }]
+    }
+}
